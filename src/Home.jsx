f2b3ffffb700b36563b4f1705ed2b5548cf80281
sunny patel <sunnykpatel.skp@gmail.com>
2023-11-08 11:37:16 -0500

    add global context

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,4 +1,8 @@
+import { useGlobalContext } from './context';
+
 const Home = () => {
+  const { openSidebar, openModal } = useGlobalContext();
+  console.log(openSidebar);
   return <div>Home</div>;
 };
 export default Home;
