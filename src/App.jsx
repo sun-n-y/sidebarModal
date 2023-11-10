0ab81fe79eb9da35a3aec88108d813117dc8add7
sunny patel <sunnykpatel.skp@gmail.com>
2023-11-10 11:40:55 -0500

    add create and import components

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,14 @@
+import Home from './Home';
+import Modal from './Modal';
+import Sidebar from './Sidebar';
+
 const App = () => {
-  return <>hello</>;
+  return (
+    <main>
+      <Home />
+      <Modal />
+      <Sidebar />
+    </main>
+  );
 };
 export default App;
