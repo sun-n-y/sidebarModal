a92c09eac1f947ee485e8048432659da65344007
sunny patel <sunnykpatel.skp@gmail.com>
2023-11-10 12:47:36 -0500

    add sidebar strucutre and css

--- src/Sidebar.jsx
+++ src/Sidebar.jsx
@@ -1,4 +1,43 @@
+import logo from './logo.svg';
+import { useContext } from 'react';
+import { social, links } from './data';
+import { FaTimes } from 'react-icons/fa';
+import { useGlobalContext } from './context';
+
 const Sidebar = () => {
-  return <div>Sidebar</div>;
+  const { isSidebarOpen, closeSidebar } = useGlobalContext();
+  return (
+    <aside className={isSidebarOpen ? 'sidebar show-sidebar' : 'sidebar'}>
+      <div className="sidebar-header">
+        <img src={logo} alt="logo" className="logo" />
+        <button className="close-btn" onClick={closeSidebar}>
+          <FaTimes />
+        </button>
+      </div>
+      <ul className="links">
+        {links.map((link) => {
+          const { url, text, icon, id } = link;
+          return (
+            <li key={id}>
+              <a href={url}>
+                {icon}
+                {text}
+              </a>
+            </li>
+          );
+        })}
+      </ul>
+      <ul className="social-links">
+        {social.map((link) => {
+          const { url, icon, id } = link;
+          return (
+            <li key={id}>
+              <a href={url}>{icon}</a>
+            </li>
+          );
+        })}
+      </ul>
+    </aside>
+  );
 };
 export default Sidebar;
